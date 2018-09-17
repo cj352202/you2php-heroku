ab1e39dec1ce20635b9d2172ed3a8a1c92e80a12
cj352202 <42927289+cj352202@users.noreply.github.com>
2018-09-17 16:15:07 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
+define('APIKEY', getenv("AIzaSyD7voYCFNvGaIw41FR0ABnkj7RZ4DiWHt8"));
+define('GJ_CODE', getenv("HK"));
+define('SITE_NAME',getenv("youtube"));
+define('TITLENAME', getenv("youtube"));
 define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('EMAIL', getenv("vivo352202@gmail.com"));
 ?>
